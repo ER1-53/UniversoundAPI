224335e1c49e54dcd6595d467ef5c235e710be4a
ER1-53 <cremey.erwan@gmail.com>
2024-08-09 18:16:16 +0200

    Update .env

--- api/src/db/sequelize.js
+++ api/src/db/sequelize.js
@@ -9,6 +9,8 @@ const songsData = require('./mock-song')
 const usersData = require('./mock-user')
 const bcrypt = require('bcrypt')
 require('dotenv').config();
+
+console.log(process.env.USER);
 // Define Sequelize instance based on environment
 let sequelize
 
@@ -16,14 +18,14 @@ if(process.env.NODE_ENV === 'production'){
   // Production environment configuration
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
     logging: true,
   });
 } else {
   // Configuration pour l'environnement de développement
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
     logging: false,
   });
 }
