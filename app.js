c5d593cd6d57a0136b32aa04e4bda7c8426f92cf
ER1-53 <cremey.erwan@gmail.com>
2024-08-11 17:03:49 +0200

    update port & dev var env

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const sequelize = require('./src/db/sequelize');
 const cors = require('cors')
 
 const app = express();
-const PORT = process.env.DB_PORT || 4001;
+const PORT = process.env.MYSQL_ADDON_PORT || 4001;
 
 app
 .use(favicon(__dirname + '/favicon.ico'))
--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -23,8 +23,8 @@ if(process.env.NODE_ENV === 'production'){
   });
 } else {
   // Configuration pour l'environnement de développement
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+  sequelize = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process.env.MYSQL_ADDON_PASSWORD, {
+    host: process.env.MYSQL_ADDON_HOST,
     dialect: 'mysql',
     logging: false,
   });
